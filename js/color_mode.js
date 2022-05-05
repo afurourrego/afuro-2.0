c67a92aef45eba1086f1e7f9f7d8d553378569d4
cristhian urrego <infoafuro@gmail.com>
2022-05-04 22:18:00 -0500

    add js color mode

---
+++ js/color_mode.js
@@ -0,0 +1,21 @@
+
+
+(function () {
+  var context = {
+    body: document.getElementById('body'),
+    btn: document.getElementById('color-mode'),
+  };
+
+  var color_mode = {
+    start: function () {
+      context.btn.addEventListener('click', color_mode.toggleColor);
+    },
+
+    toggleColor: function () {
+      context.body.classList.toggle("light");
+    },
+  };
+
+  color_mode.start();
+
+}());
